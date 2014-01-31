78c326cc6d8beb59f29ee5158950169b2c5eff24
Lukasz Sarnacki <lukesarnacki@gmail.com>
2014-01-31 16:48:47 +0100

    Adds travis_time tags to logs [WIP]
    In order to measure commands' time, they are wrapped in `time {}` block and
    its output is added to travis_time tag. travis_time can then be used to
    show execution time in html tag (like travis_fold).
    
    Conflicts:
    	lib/travis/build/script.rb
    	lib/travis/build/script/templates/header.sh
    	spec/shared/script.rb
    	spec/support/matchers.rb

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -17,6 +17,19 @@ def folds?(lines, cmd, name)
   ix_start && ix_end && lines[ix_start..ix_end].index { |line| line =~ cmd }
 end
 
+def measures_time?(lines, cmd)
+  cmd = /^(?:travis_retry )?#{Regexp.escape(cmd)}/ if cmd.is_a?(String)
+
+  icmd = lines.index { |line| line =~ cmd }
+
+  return false unless icmd
+
+  x_start = lines[icmd - 1] =~ /^echo -en travis_time:start/
+  x_end   = lines[icmd + 1] =~ /^echo -en travis_time:finish:/
+
+  x_start && x_end
+end
+
 def logs?(lines, cmd)
   # cmd = /^output from #{Regexp.escape(cmd)}/
   # lines = File.read('tmp/build.log').split("\n")
@@ -32,6 +45,11 @@ end
 def asserts?(lines, cmd)
   cmd = /^(?:travis_retry )?#{Regexp.escape(cmd)}/ if cmd.is_a?(String)
   ix = lines.index { |line| line =~ cmd }
+<<<<<<< HEAD
+=======
+  ix = ix + 1 if timeouts?(lines, cmd)
+  ix = ix + 1 if measures_time?(lines, cmd)
+>>>>>>> 9f2ce3b... Adds travis_time tags to logs [WIP]
   ix && lines[ix + 1] == "travis_assert"
 end
 
@@ -77,11 +95,19 @@ end
 
 RSpec::Matchers.define :run_script do |cmd, options = {}|
   match do |script|
+<<<<<<< HEAD
     options = options.merge(echo: true, log: true)
     expect(script).to run cmd, options
   end
   failure_message do |script|
     "expected script to run the script #{cmd.inspect} with #{options} but it didn't:\n#{log_for(script)}"
+=======
+    options = options.merge(echo: true, timing: true, log: true, timeout: timeout_for(:script))
+    failure_message_for_should do
+      "expected script to run the script #{cmd.inspect} with #{options} but it didn't:\n#{log_for(script)}"
+    end
+    script.should run cmd, options
+>>>>>>> 9f2ce3b... Adds travis_time tags to logs [WIP]
   end
 end
 
@@ -93,6 +119,7 @@ RSpec::Matchers.define :run do |cmd, options = {}|
     (!options[:log]     || logs?(lines, cmd)) &&
     (!options[:echo]    || echoes?(lines, cmd)) &&
     (!options[:retry]   || retries?(lines, cmd)) &&
+<<<<<<< HEAD
     (!options[:assert]  || asserts?(lines, cmd))
   end
   failure_message do |script|
@@ -102,6 +129,11 @@ RSpec::Matchers.define :run do |cmd, options = {}|
   failure_message_when_negated do |script|
     lines = log_for(script).split("\n")
     "expected script to not run #{cmd.inspect} with #{options} but it did:\n#{lines.join("\n")}"
+=======
+    (!options[:timing]  || measures_time?(lines, cmd)) &&
+    (!options[:assert]  || asserts?(lines, cmd)) &&
+    (!options[:timeout] || timeouts?(lines, cmd, options[:timeout]))
+>>>>>>> 9f2ce3b... Adds travis_time tags to logs [WIP]
   end
 end
 
@@ -147,5 +179,17 @@ RSpec::Matchers.define :fold do |cmd, name|
   end
   failure_message do |script|
     "expected the script to mark #{cmd} with fold markers named #{name.inspect}"
+  end
+end
+
+RSpec::Matchers.define :measures_time do |cmd|
+  match do |script|
+    lines = log_for(script).split("\n")
+
+    failure_message_for_should do
+      "expected the script to mark #{cmd} with fold markers named #{name.inspect}"
+    end
+
+    measures_time?(lines, cmd)
   end
 end
